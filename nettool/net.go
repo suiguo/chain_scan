768be459e4f50309a032cf129d5d2cf2563ebd04
Nikki <suiguo3564@gmail.com>
2022-07-29 00:55:37 +0800

    add try numbers

--- nettool/net.go
+++ nettool/net.go
@@ -2,11 +2,14 @@ package nettool
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-func GetMethod(url string, httpMethod string, method string, data []byte, head ...string) ([]byte, error) {
+var ErrorStatus error = fmt.Errorf("status error")
+
+func realRequest(url string, httpMethod string, method string, data []byte, head ...string) ([]byte, error) {
 	client := &http.Client{}
 	req, _ := http.NewRequest(httpMethod, url+method, bytes.NewBuffer(data))
 	req.Header.Add("Accept", "application/json")
@@ -21,10 +24,23 @@ func GetMethod(url string, httpMethod string, method string, data []byte, head .
 	if err != nil {
 		return nil, err
 	}
+	if resp.StatusCode != 200 {
+		return nil, ErrorStatus
+	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 	return body, nil
+}
+
+func GetMethod(url string, httpMethod string, method string, data []byte, head ...string) ([]byte, error) {
+	for i := 0; i < 5; i++ {
+		data, err := realRequest(url, httpMethod, method, data, head...)
+		if err == nil {
+			return data, err
+		}
+	}
+	return nil, fmt.Errorf("try 5 times")
 }
